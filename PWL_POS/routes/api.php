7c7dc9dc9ce4f0435a27b4a8017b1d826f31f0e9
aryawildhani13 <aryawildhaniputra@gmail.com>
2024-04-29 20:51:05 +0700

    Pertemuan 10 - Restful Api Postman Praktikum

--- PWL_POS/routes/api.php
+++ PWL_POS/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,7 +19,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
+Route::post('/logout', LogoutController::class)->name('logout');
 
-// Route::middleware('auth:api')->get('/user', function(Request $request){
-//     return $request->user();
-// });
+Route::get('/levels', [LevelController::class, 'index']);
+Route::post('/levels', [LevelController::class, 'store']);
+Route::get('/levels/{level}', [LevelController::class, 'show']);
+Route::put('/levels/{level}', [LevelController::class, 'update']);
+Route::delete('/levels/{level}', [LevelController::class, 'destroy']);
+
+Route::middleware('auth:api')->get('/user', function(Request $request){
+    
+    return $request->user();
+});
