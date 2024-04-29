bf2deb7cb7f0eeb9b9bbfc762abf3fecc5d706cd
aryawildhani13 <aryawildhaniputra@gmail.com>
2024-04-29 18:12:04 +0700

    Pertemuan 9 - Login Auth

--- PWL_POS/app/Models/UserModel.php
+++ PWL_POS/app/Models/UserModel.php
@@ -3,23 +3,39 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class UserModel extends Model
+class userModel extends Authenticatable
 {
     use HasFactory;
 
     protected $table = 'm_user';
+    public $timestamps = true;
     protected $primaryKey = 'user_id';
 
-    protected $fillable = [
-        'level_id',
-        'username',
-        'nama',
+    protected $fillable =  [
+        'user_id', 
+        'level_id', 
+        'username', 
+        'nama', 
         'password',
+        'profile_img',
+        'status'
     ];
-    public function level(): BelongsTo 
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+
+    public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
